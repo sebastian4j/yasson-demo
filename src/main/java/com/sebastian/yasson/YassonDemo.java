18a761fe659920a6b1b139cbcafcbf583b5e2ef5
sebastian <sebastian@mnjr>
2018-05-06 13:20:10 -0300

    imports no requeridos

--- src/main/java/com/sebastian/yasson/YassonDemo.java
+++ src/main/java/com/sebastian/yasson/YassonDemo.java
@@ -2,11 +2,7 @@ package com.sebastian.yasson;
 
 import java.io.InputStream;
 import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
-import javax.json.stream.JsonParser;
 
 /**
  * demo usando jsonp y jsonb
@@ -26,23 +22,23 @@ public class YassonDemo {
     }
 
     private static void jsonb() {
-        Jsonb jb = JsonbBuilder.create();
-        Persona p = jb.fromJson(leer(), Persona.class);
+        var jb = JsonbBuilder.create();
+        var p = jb.fromJson(leer(), Persona.class);
         System.out.println(p);
         System.out.println(jb.toJson(p));
     }
 
     private static void jsonp() {
         // memoria
-        JsonReader reader = Json.createReader(leer());
-        JsonObject jobj = reader.readObject();
+        var reader = Json.createReader(leer());
+        var jobj = reader.readObject();
         System.out.println(jobj);
-        JsonObject json = Json.createObjectBuilder().add("nombre", "Elías").build();
+        var json = Json.createObjectBuilder().add("nombre", "Elías").build();
         System.out.println(json);
         // eventos
-        JsonParser parser = Json.createParser(YassonDemo.class.getClassLoader().getResourceAsStream("persona.json"));
+        var parser = Json.createParser(YassonDemo.class.getClassLoader().getResourceAsStream("persona.json"));
         while (parser.hasNext()) {
-            JsonParser.Event e = parser.next();
+            var e = parser.next();
             System.out.print(e.name());
             switch (e) {
                 case KEY_NAME:
